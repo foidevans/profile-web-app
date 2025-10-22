9f3c9c806b6ac6deac929b7507970e7ab7c84f0d
Favour Evans <divfavour04@gmail.com>
2025-10-22 23:25:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# 🪪 Testable Profile Card
+
+A modern and responsive **Profile Card Web App** built wsimple HTML, CSS, and JavaScript p
+
+---
+
+## 🚀 Features
+
+- 🧭 **Navigation Bar** for seamless page switching  
+- 🧑‍💻 **Profile Card Section** showcasing basic user info  
+- ℹ️ **About Page** with detailed information  
+- 📞 **Contact Page** for connecting with the user  
+- 💅 **Responsive Design** using modern CSS flexbox  
+- 🪞 **Glassmorphism Design**: Soft, blurred background with subtle transparency.
+
+
+---
+
+## 🛠️ Built With
+
+- **HTML**
+- **Vanilla JS**
+- **CSS3 (Flexbox & Responsive Design)**
+
+---
+
+
+
+## ⚙️ Installation & Setup
+
+To run this project locally:
+
+```bash
+# Clone the repository
+git clone https://github.com/foidevans/Testable-profile-card.git
+
+# Navigate to the project folder
+cd Testable-profile-card
+
+👩‍💻 Author
+
+Favour Evans
+Frontend Developer | Web3 Enthusiast
+
+
+🪄 Acknowledgements
+
+Special thanks to HNG and everyone who contributed feedback on this project.
---
+++ script.js
@@ -0,0 +1,55 @@
+const dateContainer = document.querySelector(".current-date");
+const p = document.createElement("p");
+dateContainer.appendChild(p);
+
+const updateDate = () => {
+  const now = new Date();
+  const timeString = `${now.toLocaleTimeString()}.${now.getMilliseconds()}`;
+  p.textContent = timeString;
+};
+
+updateDate();
+
+setInterval(updateDate, 1000);
+
+const form = document.getElementById("contactForm");
+const successDiv = document.getElementById("successMessage");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const subject = document.getElementById("subject").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  const errors = {};
+
+  if (!name) errors.name = "Full name is required.";
+  if (!email) errors.email = "Email is required.";
+  else if (!/\S+@\S+\.\S+/.test(email)) errors.email = "Enter a valid email.";
+  if (!subject) errors.subject = "Subject is required.";
+  if (!message) errors.message = "Message is required.";
+  else if (message.length < 10)
+    errors.message = "Message must be at least 10 characters.";
+
+
+  ["name", "email", "subject", "message"].forEach((id) => {
+    const errorDiv = document.getElementById("error-" + id);
+    if (errors[id]) {
+      errorDiv.innerText = errors[id];
+      errorDiv.style.display = "block";
+    } else {
+      errorDiv.innerText = "";
+      errorDiv.style.display = "none";
+    }
+  });
+
+  if (Object.keys(errors).length === 0) {
+    successDiv.style.display = "block";
+    setTimeout(() => (successDiv.style.display = "none"), 3000);
+    form.reset();
+  } else {
+    successDiv.style.display = "none";
+  }
+});
